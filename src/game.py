f78e55b15d50079b31ef54c0841b8b28a6f83db9
amirhossein <pinoq@protonmail.com>
2021-06-22 16:57:35 +0430

    added game module and game interface abstract class

---
+++ src/game.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class GameInterface(ABC):
+    @abstractmethod
+    def reset_game():
+        pass
+
+    @abstractmethod
+    def get_table():
+        pass
+
+    @abstractmethod
+    def get_row():
+        pass
+
+    @abstractmethod
+    def get_column():
+        pass
+
+    @abstractmethod
+    def get_rtl_diagonal():
+        pass
+
+    @abstractmethod
+    def get_ltr_diagonal():
+        pass
