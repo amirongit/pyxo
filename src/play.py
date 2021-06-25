ec5e084d493575db50d711e47f1145a7aff7a150
amirhossein <pinoq@protonmail.com>
2021-06-25 14:28:08 +0430

    added player class to play module

--- src/play.py
+++ src/play.py
@@ -1,1 +1,14 @@
-from interfaces import PlayerInterface
+from interfaces import PlayerInterface, GameInterface
+
+
+class Player(PlayerInterface):
+    def __init__(self, game: GameInterface, char: str):
+        self._game = game
+        self.char = char
+
+    @property
+    def game(self) -> GameInterface:
+        return self._game
+
+    def mark(self, point: tuple):
+        self.game.set_point(point, self.char)
